b91388653baf26b25f6243f9f5723699c0675858
Mechislav <id110099122@gmail.com>
2021-01-12 17:54:42 +0200

    1.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,22 @@
-import {PathLike} from 'fs';
-import fs, {FileHandle} from 'fs/promises';
+import { PathLike } from 'fs';
+import fs, { FileHandle } from 'fs/promises';
 
 type FileStreamOptions = {
-  bufferSize?: number,
-  bufferLimit?: number,
-  flags?: string,
-  terminator?: string | Buffer,
-  includeTerminator?: boolean,
-  transform?: (e: Buffer) => Promise<any>,
+  bufferSize?: number;
+  bufferLimit?: number;
+  flags?: string;
+  terminator?: string | Buffer;
+  includeTerminator?: boolean;
+  transform?: (e: Buffer) => Promise<any>;
 };
 
 export async function* afstream(path: FileHandle | PathLike, options?: FileStreamOptions): AsyncGenerator<Buffer> {
-
   options = options || {};
 
   const bufferLimit = options.bufferLimit || 0;
   const terminator = options.terminator || '\n';
   const includeTerminator = options.includeTerminator || false;
-  const transform = options.transform || (async e => e);
+  const transform = options.transform || (async (e) => e);
   const flags = options.flags || 'r';
   let bufferSize = options.bufferSize || 0;
 
@@ -38,17 +37,17 @@ export async function* afstream(path: FileHandle | PathLike, options?: FileStrea
   let buffer = Buffer.alloc(0);
   let offset = 0;
 
-  bufferSize = Math.min(((bufferSize as number) || stat.blksize || 1024), stat.size);
+  bufferSize = Math.min((bufferSize as number) || stat.blksize || 1024, stat.size);
   const terminatorBuffer = terminator ? (Buffer.isBuffer(terminator) ? terminator : Buffer.from(terminator)) : null;
 
   while (offset < stat.size) {
     const dataBufferSize = Math.min(stat.size - offset, bufferSize);
 
-    if (bufferLimit && (buffer.length + dataBufferSize) > bufferLimit) {
+    if (bufferLimit && buffer.length + dataBufferSize > bufferLimit) {
       throw new Error(`Cannot allocate additional buffer of size ${dataBufferSize} limit of ${bufferLimit} reached`);
     }
 
-    const data = (await handle.read(Buffer.alloc(dataBufferSize), 0, dataBufferSize, offset));
+    const data = await handle.read(Buffer.alloc(dataBufferSize), 0, dataBufferSize, offset);
     offset += data.bytesRead;
 
     if (!terminatorBuffer) {
@@ -60,7 +59,7 @@ export async function* afstream(path: FileHandle | PathLike, options?: FileStrea
 
     let terminatorIndex;
     while ((terminatorIndex = buffer.indexOf(terminatorBuffer)) > -1) {
-      yield await transform(buffer.slice(0, (terminatorIndex - (includeTerminator ? 0 : terminatorBuffer.byteLength))));
+      yield await transform(buffer.slice(0, terminatorIndex - (includeTerminator ? 0 : terminatorBuffer.byteLength)));
       buffer = buffer.slice(terminatorIndex + terminatorBuffer.byteLength);
     }
   }
